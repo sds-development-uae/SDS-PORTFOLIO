00a4b46bf29a39f20e29d5000cd9d6ef5642c505
sumangaldey8972 <sumangaldey8972@gmail.com>
2026-01-21 17:47:08 +0530

    production build ready

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,6 @@ import Process from "@/components/Process";
 import Services from "@/components/Services";
 import Testimonials from "@/components/Testimonials";
 import Work from "@/components/Work";
-import Image from "next/image";
 
 export default function Home() {
   return (
